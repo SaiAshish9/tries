81811607b222a59bd8bee51cdd0940522d583919
SaiAshish <saiashish3760@gmail.com>
2024-04-17 22:23:59 +0530

    Add files via upload

--- Trie.java
+++ Trie.java
@@ -67,3 +67,14 @@ public class Trie {
     }
 }
 
+//                root
+//                / \
+//                a   NULL
+//                /
+//                p
+//                / \
+//                p   NULL
+//                /
+//                l
+//                /
+//                e
